d6c7ca078dd03ef5c856000aa6162d7d49b3d5a1
vo <tien113@gmail.com>
2012-10-05 12:35:08 +0300

    before layout.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,11 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.8'
-gem 'sqlite3','1.3.6'
+
+group :development, :test do
+  gem 'sqlite3','1.3.6'
+  gem 'rspec-rails', '2.11.0'
+end
 
 group :assets do
   gem 'sass-rails', '3.2.5'
@@ -16,6 +20,14 @@ gem 'jquery-rails', '2.1.3'
 gem 'bcrypt-ruby', '3.0.1'
 
 gem "will_paginate", '3.0.3'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+group :production do
+  gem 'pg', '0.14.1'
+end
 
 # To use on Linux Server
 gem 'execjs'
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
-  
+  has_many :foods
   attr_accessible :place_name, :place_lat, :place_log, :place_address,
                   :place_opening_time_1, :place_opening_time_2, :place_opening_time_3, :place_opening_time_4,
                   :place_phone_number, :place_email, :place_image_url
@@ -12,9 +12,7 @@ class Place < ActiveRecord::Base
   validates :place_email, :presence => true, :length => 1..255
   validates :place_image_url, :presence => true, :length => 1..255
 
-  validates_uniqueness_of :place_email
-  validates_format_of :place_email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i,
-                                    :message => "Invalid email"
-    
-  has_many :foods
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :place_email, format: { with: VALID_EMAIL_REGEX },
+                          uniqueness: { case_sensitive: false }
 end
