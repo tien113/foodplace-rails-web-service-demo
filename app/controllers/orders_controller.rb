ac25794ab054d8c5b8434749fd9be1b753f21dd3
vo <tien113@gmail.com>
2012-10-06 21:32:53 +0300

    fixing orders.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,11 +15,6 @@ class OrdersController < ApplicationController
   # GET /orders/1.json
   def show
     @order = Order.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @order }
-    end
   end
 
   # GET /orders/1/edit
@@ -34,11 +29,7 @@ class OrdersController < ApplicationController
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render json: @order, status: :created, location: @order }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
+        format.json { render json: @order, status: :created }
       end
     end
   end
@@ -48,26 +39,19 @@ class OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
 
-    respond_to do |format|
-      if @order.update_attributes(params[:order])
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
+    if @order.update_attributes(params[:order])
+     redirect_to @order
+     flash[:success] = "Order was successfully updated."  
+    else
+      render action: "edit" 
     end
   end
 
   # DELETE /orders/1
   # DELETE /orders/1.json
   def destroy
-    @order = Order.find(params[:id])
-    @order.destroy
-
-    respond_to do |format|
-      format.html { redirect_to orders_url }
-      format.json { head :no_content }
-    end
+    @order = Order.find(params[:id]).destroy
+    flash[:success] = "Order destroyed."
+    redirect_to orders_url
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,9 +6,9 @@ class Order < ActiveRecord::Base
   has_many :order_details, :dependent => :delete_all
   accepts_nested_attributes_for :order_details
   
-  validates :order_uuid, :presence => true, :length => 1..255
-  validates :order_total, :presence => true, :length => 1..8
-  validates :order_date, :presence => true, :length => 1..255
-  validates :order_details, :presence => true
+  validates :order_uuid, length: 1..255
+  validates :order_total, length: 1..8
+  validates :order_date, length: 1..255
+  validates :order_details, presence: true
   
 end
