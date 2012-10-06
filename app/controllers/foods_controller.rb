17d51cd640de10f3918379b9d7f6145b4d26e62b
vo <tien113@gmail.com>
2012-10-06 15:12:35 +0300

    done food.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -20,48 +20,29 @@ class FoodsController < ApplicationController
   # GET /foods/1.json
   def show
     @food = Food.find(params[:id]) # find the food with id
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @food } # generate the data to json
-    end
   end
 
   # GET /foods/new
   # GET /foods/new.json
   def new
-    @food = Food.new # create new food 
-    @places = Place.all # fetch all places
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @food }
-    end
+    @food = Food.new # create new food
   end
 
   # GET /foods/1/edit
   def edit
     @food = Food.find(params[:id]) # edit the food with id
-    @places = Place.all
   end
 
   # POST /foods
   # POST /foods.json
   def create
     @food = Food.new(params[:food]) # create the food with food data
-    @places = Place.all
-
-    respond_to do |format|
-      if @food.save
-        format.html { 
-          redirect_to @food
-          flash[:success] = "Food was successfully created." }
-        
-        format.json { render json: @food, status: :created, location: @food }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
+    
+    if @food.save
+      redirect_to @food
+      flash[:success] = "Food was successfully created."
+    else
+      render action: "new"
     end
   end
 
@@ -70,29 +51,19 @@ class FoodsController < ApplicationController
   def update
     @food = Food.find(params[:id]) # find food with id
 
-    respond_to do |format|
-      if @food.update_attributes(params[:food])
-        format.html { 
-          redirect_to @food
-          flash[:success] = "Food was successfully updated." }
-        
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
+    if @food.update_attributes(params[:food])
+        redirect_to @food
+        flash[:success] = "Food was successfully updated." 
+    else 
+      render action: "edit"
     end
   end
 
   # DELETE /foods/1
   # DELETE /foods/1.json
   def destroy
-    @food = Food.find(params[:id])
-    @food.destroy # destroy food
-
-    respond_to do |format|
-      format.html { redirect_to foods_url }
-      format.json { head :no_content }
-    end
+    @food = Food.find(params[:id]).destroy # destroy food
+    flash[:success] = "Food destroyed."
+    redirect_to foods_url
   end  
 end
