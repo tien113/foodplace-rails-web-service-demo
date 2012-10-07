0a43117dac800456484961199ab0ce4f11ceabdf
vo <tien113@gmail.com>
2012-10-08 00:17:24 +0300

    fix space.

--- db/migrate/20120513103807_create_orders.rb
+++ db/migrate/20120513103807_create_orders.rb
@@ -1,9 +1,9 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.string :order_uuid
+      t.string  :order_uuid
       t.decimal :order_total, :precision => 7, :scale => 2
-      t.string :order_date
+      t.string  :order_date
       t.boolean :order_done
       
       t.timestamps
