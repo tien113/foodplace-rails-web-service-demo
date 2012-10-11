02ffe93194b09490196672c388360ce5c96b4dff
vo <tien113@gmail.com>
2012-10-12 01:14:01 +0300

    fix sign in user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,13 +50,6 @@ class UsersController < ApplicationController
   
   private
     
-    def signed_in_user
-      unless signed_in? 
-        store_location
-        redirect_to signin_url, notice: "You need to sign in first."
-      end
-    end
-    
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,4 +34,11 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url
   end
+    
+  def signed_in_user
+    unless signed_in? 
+      store_location
+      redirect_to signin_url, notice: "You need to sign in first."
+    end
+  end
 end
